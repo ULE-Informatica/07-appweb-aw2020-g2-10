b1c91910976e1d78da47dc84d0a8387a9bf7acf2
Jorge García Mateos <jgarcm16@estudiantes.unileon.es>
2020-05-12 19:03:13 +0200

    Initial config

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+var cors = require('cors');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(cors)
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
